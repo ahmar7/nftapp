1f20e4fdd176abd113accd26d67016d85954cb96
A. Developers <ahmarjabbar7@gmail.com>
2022-12-11 12:24:56 +0500

    initial

---
+++ src/components/nft.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./nft.css";
+const Nft = () => {
+  return (
+    <div className="nft-section">
+      <div className="nft-flex">
+        <div className="content-nft">
+          <h1>
+            {" "}
+            GET IN EARLY ON THE BLUE SOCIAL TOKEN PRE-SALE. BUY THE $BST TOKEN
+            NOW!
+          </h1>
+          <p>
+            The $BST tokens is the currency that powers the Blue Bree T
+            ToRoLateoy e/a oigel ela)
+          </p>
+          <p>
+            You can buy directly with your USDT, or you can buy with the Eth in
+            your wallet. After the private pre-sale ends. You can use the claim
+            page to claim your purchased BST.
+          </p>
+          <p>
+            Mer SERUM at Molalla aioli Umer Cy le keine) SRS ISU CUR Cols [oy 7)
+          </p>
+        </div>
+        <div className="content-card"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Nft;
